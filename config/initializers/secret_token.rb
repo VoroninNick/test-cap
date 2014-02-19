3c863616a00898468dd5fff698c9f7f9c3536ea7
pasha <p.korenev@voroninstudio.eu>
2014-02-19 16:02:20 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestCap::Application.config.secret_key_base = 'c26a9c36f9512b7cc2e5a30c48fbd2965d339ca11feef107ab5688dea88f6cb0925b97f18d5b281755316e5d0aa092cfde90963d21895be6b57d199104ee6e69'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestCap::Application.config.session_store :cookie_store, key: '_test-cap_session'
